29aea2a0d0dcd139545b8bb87aed760171f05f0e
Vincenzo Ruggiero <45271214+VincenzoRuggiero@users.noreply.github.com>
2023-06-14 12:36:38 +0200

    Create header as a component

---
+++ templates/header.php
@@ -0,0 +1,17 @@
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/css/materialize.min.css">
+    <link rel="stylesheet" href="style.css">
+    <title>PHPizza</title>
+</head>
+
+<body class="grey lighten-4">
+    <nav class="white z-depth-0">
+        <div class="container">
+            <a href="#" class="brand-logo brand-text">PHPizza</a>
+            <ul id="nav-mobile" class="right hide-on-small-and-down">
+                <li><a href="#" class="btn brand z-depth-0">Aggiungi pizza</a></li>
+            </ul>
+        </div>
+    </nav>
